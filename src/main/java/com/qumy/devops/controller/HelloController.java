0d1feb7094aaf45e1ce9a5d4fd7c575984472dee
qumy <17634620196@163.com>
2022-03-20 20:29:03 +0800

    新增： 1. hello 接口。

---
+++ src/main/java/com/qumy/devops/controller/HelloController.java
@@ -0,0 +1,17 @@
+﻿package com.qumy.devops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Administrator
+ * @since 2022/3/20 20:27
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello DevOps!!!";
+    }
+}
