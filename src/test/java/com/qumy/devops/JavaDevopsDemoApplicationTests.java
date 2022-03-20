89614cb7f3c0e785076869f5ed1f379e8564f2e9
qumy <17634620196@163.com>
2022-03-20 20:10:45 +0800

    Initial commit

---
+++ src/test/java/com/qumy/devops/JavaDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qumy.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevopsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
